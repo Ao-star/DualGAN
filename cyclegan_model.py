ceb0958c9c0092919c10cb3feff5da299c58b0f8
Ao-star <93997027+Ao-star@users.noreply.github.com>
2024-05-06 10:45:27 +0800

    Update cyclegan_model.py

--- cyclegan_model.py
+++ cyclegan_model.py
@@ -2,21 +2,126 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import Dataset, DataLoader
-
+import os
+from torchvision.datasets.folder import IMG_EXTENSIONS
+from consts import SPLIT_TRAIN
+from osgeo import gdal
+import numpy as np
+import PIL.Image
+from torchvision import transforms
 
 # 定义数据集
-class CycleGANDataset(Dataset):
-    def __init__(self, path):
-        # 加载数据集
-        ...
+class ImageDomainFolder(Dataset):
+    """Dataset structure introduced in a CycleGAN paper.
+
+    This dataset expects images to be arranged into subdirectories
+    under `path`: `trainA`, `trainB`, `testA`, `testB`. Here, `trainA`
+    subdirectory contains training images from domain "a", `trainB`
+    subdirectory contains training images from domain "b", and so on.
+
+    Parameters
+    ----------
+    path : str
+        Path where the dataset is located.
+    domain : str
+        Choices: 'a', 'b'.
+    split : str
+        Choices: 'train', 'test', 'val'
+    transform : Callable or None,
+        Optional transformation to apply to images.
+        E.g. torchvision.transforms.RandomCrop.
+        Default: None
+    """
+
+    def __init__(
+        self, path,
+        domain        = 'a',
+        split         = SPLIT_TRAIN,
+        transform     = None,
+        **kwargs
+    ):
+        super().__init__(**kwargs)
+
+        subdir = split + domain
+        self._path      = os.path.join(path, subdir)
+        self._imgs      = ImageDomainFolder.find_images_in_dir(self._path)
+        self._transform = transform
+
+    @staticmethod
+    def find_images_in_dir(path):
+        extensions = set(IMG_EXTENSIONS)
+
+        result = []
+        for fname in os.listdir(path):
+            fullpath = os.path.join(path, fname)
+
+            if not os.path.isfile(fullpath):
+                continue
+
+            ext = os.path.splitext(fname)[1]
+            if ext not in extensions:
+                continue
+
+            result.append(fullpath)
+
+        result.sort()
+        return result
 
     def __len__(self):
-        # 返回数据集大小
-        ...
+        return len(self._imgs)
 
     def __getitem__(self, index):
-        # 返回指定索引处的数据
-        ...
+        path   = self._imgs[index]
+        result = self.loader_image(path)
+
+        if self._transform is not None:
+            result = self._transform(result)
+        return result
+        
+    def loader_image(self, path):
+        dataset = gdal.Open(path, gdal.GA_ReadOnly).ReadAsArray().astype('float32')
+        if dataset is None:
+            raise FileNotFoundError(f"Could not open {path}")
+
+        print(torch.from_numpy(dataset))
+        return torch.from_numpy(dataset)
+
+
+class ImageDomainHierarchy(Dataset):
+
+    def __init__(
+        self, path, domain,
+        split=SPLIT_TRAIN,
+        transform=None,
+        **kwargs
+    ):
+        super().__init__(**kwargs)
+
+        self._path = os.path.join(path, split, domain)
+        self._imgs = ImageDomainFolder.find_images_in_dir(self._path)
+        self._transform = transform
+
+    def __len__(self):
+        return len(self._imgs)
+
+    def __getitem__(self, index):
+        path = self._imgs[index]
+        result = PIL.Image.fromarray(self.loader_image(path))
+
+        if self._transform is not None:
+            result = self._transform(result)
+        return result
+
+    def loader_image(self, path):
+        dataset = gdal.Open(path, gdal.GA_ReadOnly).ReadAsArray().astype('float32')
+        if dataset is None:
+            raise FileNotFoundError(f"Could not open {path}")
+        if dataset.ndim == 3:
+            return np.uint8(dataset.swapaxes(0,1).swapaxes(1,2))
+        else:
+            # return np.uint8(dataset)
+            mat = np.uint8(dataset)[:,:,np.newaxis]
+            return np.concatenate((mat,mat,mat),axis=2)
         
 # 定义生成器
 class Generator(nn.Module):
