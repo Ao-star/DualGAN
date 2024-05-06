cbc4b8b34ff5455d23665f61b74753ec07a88de2
Ao-star <93997027+Ao-star@users.noreply.github.com>
2024-05-06 10:25:28 +0800

    Update cyclegan_model.py

--- cyclegan_model.py
+++ cyclegan_model.py
@@ -3,11 +3,21 @@ import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import Dataset, DataLoader
 
-from osgeo import gdal
-import numpy as np
-import os
 
+# 定义数据集
+class CycleGANDataset(Dataset):
+    def __init__(self, path):
+        # 加载数据集
+        ...
 
+    def __len__(self):
+        # 返回数据集大小
+        ...
+
+    def __getitem__(self, index):
+        # 返回指定索引处的数据
+        ...
+        
 # 定义生成器
 class Generator(nn.Module):
     def __init__(self):
@@ -57,11 +67,13 @@ class CycleGAN(nn.Module):
 def train():
     dataset_A = CycleGANDataset('path/to/dataset_A')
     dataset_B = CycleGANDataset('path/to/dataset_B')
-    dataloader_A = DataLoader(dataset_A, batch_size=1, shuffle=True)
-    dataloader_B = DataLoader(dataset_B, batch_size=1, shuffle=True)
+    dataloader_A = DataLoader(dataset_A, batch_size=batch_size, shuffle=True)
+    dataloader_B = DataLoader(dataset_B, batch_size=batch_size, shuffle=True)
 
     model = CycleGAN()
-    optimizer = optim.Adam(model.parameters(), lr=0.0002)
+    optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+
+    criterion = nn.MSELoss()
 
     for epoch in range(num_epochs):
         for i, (real_A, real_B) in enumerate(zip(dataloader_A, dataloader_B)):
@@ -69,12 +81,32 @@ def train():
             fake_B = model.generator_A2B(real_A)
 
             # 计算判别器的损失
-            ...
+            pred_real_A = model.discriminator_A(real_A)
+            pred_fake_A = model.discriminator_A(fake_A.detach())
+            loss_discriminator_A = criterion(pred_real_A, torch.ones_like(pred_real_A)) + criterion(pred_fake_A, torch.zeros_like(pred_fake_A))
+
+            pred_real_B = model.discriminator_B(real_B)
+            pred_fake_B = model.discriminator_B(fake_B.detach())
+            loss_discriminator_B = criterion(pred_real_B, torch.ones_like(pred_real_B)) + criterion(pred_fake_B, torch.zeros_like(pred_fake_B))
+
+            loss_discriminator = 0.5 * (loss_discriminator_A + loss_discriminator_B)
 
             # 计算生成器的损失
-            ...
+            pred_fake_A = model.discriminator_A(fake_A)
+            pred_fake_B = model.discriminator_B(fake_B)
+            loss_generator_A2B = criterion(pred_fake_B, torch.ones_like(pred_fake_B))
+            loss_generator_B2A = criterion(pred_fake_A, torch.ones_like(pred_fake_A))
+
+            # 计算循环一致性损失
+            recovered_A = model.generator_B2A(fake_B)
+            recovered_B = model.generator_A2B(fake_A)
+            loss_cycle_A = criterion(recovered_A, real_A)
+            loss_cycle_B = criterion(recovered_B, real_B)
+
+            loss_generator = loss_generator_A2B + loss_generator_B2A + 10 * (loss_cycle_A + loss_cycle_B)
 
             # 更新网络权重
             optimizer.zero_grad()
-            loss.backward()
+            loss_discriminator.backward(retain_graph=True)
+            loss_generator.backward()
             optimizer.step()
